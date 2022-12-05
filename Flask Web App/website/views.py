2dcdf60bb074bfa9eaf51b9a13238f7dcbd87ac9
Shane O'G <G00411304@gmit.ie>
2022-12-05 16:33:39 +0000

    Navbar and pages added - https://www.youtube.com/watch?v=dam0GPOAvVI&t=982s&ab_channel=TechWithTim

--- Flask Web App/website/views.py
+++ Flask Web App/website/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
-@views
-#16:22
-
+@views.route('/')
+def home():
+    return render_template("home.html")
